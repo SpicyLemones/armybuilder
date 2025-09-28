e340abfab3b96e6442d35e5493af988869e19701
Kenzie <ktan9755@uni.sydney.edu.au>
2025-09-29 00:47:42 +1000

    s

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/pricehammer',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
